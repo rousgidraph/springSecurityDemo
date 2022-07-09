76c5b7426a9f0fb2975fbb3f99383ef28da3a5b3
rousgidraph <rousgidraph@gmail.com>
2022-07-09 22:37:49 +0300

    Working commmit, 0 code coverage

--- README.md
+++ README.md
@@ -5,5 +5,4 @@
 
 [Reference Video](https://youtu.be/VVn9OG9nfH0)
 
-### Quote
-:coffee:
+To Login hit up : localhost:8080/login and the dataType is x-www-form-urlencoded
--- src/main/java/com/gidraph/springsecuritydemo/security/securityConfiguration.java
+++ src/main/java/com/gidraph/springsecuritydemo/security/securityConfiguration.java
@@ -1,8 +1,11 @@
 package com.gidraph.springsecuritydemo.security;
 
 import com.gidraph.springsecuritydemo.filter.CustomAuthenticationFilter;
+import com.gidraph.springsecuritydemo.filter.CustomAuthorisationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @Configuration
 @EnableWebSecurity
@@ -27,12 +36,24 @@ public class securityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter filter = new CustomAuthenticationFilter(authenticationManagerBean());
+        filter.setFilterProcessesUrl("/api/login");
+        http.addFilterBefore(new CustomAuthorisationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter());
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().antMatchers(GET,"/api/login/**","/api/token/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers(GET,"/api/user/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST,"/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(filter);
+
 
     }
 
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean()throws Exception{
+        return super.authenticationManagerBean();
+    }
 
 }
